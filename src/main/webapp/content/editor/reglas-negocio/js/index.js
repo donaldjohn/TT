3f4956ef5a0c396d39f29eb442b5841307620df4
Natalia <hdeznatali@gmail.com>
2015-08-17 17:11:35 -0700

    Correccion de eliminacion de rn

--- src/main/webapp/content/editor/reglas-negocio/js/index.js
+++ src/main/webapp/content/editor/reglas-negocio/js/index.js
@@ -22,34 +22,38 @@ function verificarEliminacionElemento(idElemento) {
 			idSel : idElemento
 		},
 		success : function(data) {
-			console.log("data: " + data);
-			mostrarMensajeEliminacion(data);
+			mostrarMensajeEliminacion(data, idElemento);
 		},
 		error : function(err) {
-			console.log("AJAX error in request: " + JSON.stringify(err, null, 2));
-			alert("Error");
+			alert("AJAX error in request: " + JSON.stringify(err, null, 2));
 		}
 	});
+	
 	return false;
 	
 }
 
-function mostrarMensajeEliminacion(json) {
-	
+function mostrarMensajeEliminacion(json, id) {
 	var elementos = document.createElement("ul");
+	var elementosReferencias = document.getElementById("elementosReferencias");
+	var urlEliminar = contextPath + "/reglas-negocio/" +id+ "?_method=delete";
+	while (elementosReferencias.firstChild) {
+		elementosReferencias.removeChild(elementosReferencias.firstChild);
+	}
 	if (json != "") {
 		$
 				.each(
 						json,
 						function(i, item) {
 							var elemento = document.createElement("li");
-							elemento.appendChild(document.createTextNode(item.clave + item.numero + " " + item.nombre));
+							elemento.appendChild(document.createTextNode(item));
 							elementos.appendChild(elemento);
 						});
 		document.getElementById("elementosReferencias").appendChild(elementos);
 		
 		$('#mensajeReferenciasDialog').dialog('open');
-	} else {
+	} else {	
+		document.getElementById("btnConfirmarEliminacion").onclick = function(){ confirmarEliminacion(urlEliminar);};
 		$('#confirmarEliminacionDialog').dialog('open');
 	}
 }
