adcf5c315ab1aa7c23ce7333cd4e17acf405caec
sramirezc <sramirezc@192.168.2.1>
2015-11-26 16:59:24 -0600

    u

--- src/main/java/mx/prisma/util/Test.java
+++ src/main/java/mx/prisma/util/Test.java
@@ -7,7 +7,7 @@ public class Test {
 
 	public static void main(String[] args) throws Exception {
 		
-		GeneradorPruebasBs.generarCasosPrueba(new CasoUsoDAO().consultarCasoUso(48), "");
+		//GeneradorPruebasBs.generarCasosPrueba(new CasoUsoDAO().consultarCasoUso(48), "");
 		
 	}
 	
